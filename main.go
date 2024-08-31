8a7c77fef12224a5dbb4f66bea6932eb90f5118d
elameen <elameendaiyabu@gmail.com>
2024-08-31 23:01:29 +0000

    edited main

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	var folder string
 	var email string
 	flag.StringVar(&folder, "add", "", "add new folder to scan for git repositories")
-	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.StringVar(&email, "email", "", "the email to scan")
 	flag.Parse()
 
 	if folder != "" {
